4113393024549dc782bf25c0588deaa966310f87
Roger Pender <rdpender3@gmail.com>
2023-02-01 14:18:03 -0800

    adding logo and basic styles for nav

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import logo from "../../assets/icons8-comet.png"
+import "./navigation.styles.scss"
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <div>Logo</div>
+                    <img src={logo} className='logo' />
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">
