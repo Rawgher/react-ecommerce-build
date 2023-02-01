05d9201428e75db1358dd58056fce3f36056220f
Roger Pender <rdpender3@gmail.com>
2023-02-01 13:58:48 -0800

    basic routing tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am a nav bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop page</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        {/* index means to match the base component, so / would also equate to home */}
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
