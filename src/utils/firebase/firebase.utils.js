bbc9ef61610a56e9de7c48c08f6af71eb81f10fd
Roger Pender <rdpender3@gmail.com>
2023-02-03 10:50:58 -0800

    working on user auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA0Z1axsF4ZAmGR3FExNKh3_HOSVkuvsqw',
@@ -20,4 +21,16 @@ export const auth = getAuth();
 
 export const signInWithGooglePopup = () => {
   return signInWithPopup(auth, provider);
+};
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.user.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
 };
