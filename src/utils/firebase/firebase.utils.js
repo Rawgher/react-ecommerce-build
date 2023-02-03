c4c9c93190aa73d486225249eaf3eaf703528b74
Roger Pender <rdpender3@gmail.com>
2023-02-02 17:13:05 -0800

    working with firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,3 +1,23 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
-const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: 'AIzaSyA0Z1axsF4ZAmGR3FExNKh3_HOSVkuvsqw',
+  authDomain: 'comet-co-db.firebaseapp.com',
+  projectId: 'comet-co-db',
+  storageBucket: 'comet-co-db.appspot.com',
+  messagingSenderId: '43773240311',
+  appId: '1:43773240311:web:968e2878f71c1c4fc0c4f2',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => {
+  return signInWithPopup(auth, provider);
+};
