a2d2fa20dd55653b44aaee8c7046b5b9cd9bb55f
Roger Pender <rdpender3@gmail.com>
2023-02-23 14:41:43 -0800

    getting separate category pages showing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         {/* index means to match the base component, so / would also equate to home */}
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
